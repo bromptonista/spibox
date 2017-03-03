52ea813602e5ddfea8e1d6980f7aae90f6b30ed6
bromptonista <bromptonista@yahoo.com>
2017-03-03 15:43:35 +0000

    0.1 Added video
    Added code to take a 25sec video. Increased picture quality to 5MP

--- spibox.py
+++ spibox.py
@@ -12,21 +12,28 @@ def photo():
     for i in range(1,2):
         capturename = get_file_name()
         print('Motion detected! Taking snapshot')
-        cmd="raspistill -w 640 -h 480 -n -t 10 -q 10 -e jpg -th none -o /home/pi/spibox/capture/" + capturename+"_%d.jpg" % (i)
+        # Changed settings to 5MP - original was 640x480 pictures, see
+        # https://www.raspberrypi.org/documentation/usage/camera/raspicam/raspistill.md
+        # for documentation of raspberry pi camera use for stills
+        cmd="raspistill -w 2592 -h 1944 -n -t 10 -q 10 -e jpg -th none -o /home/pi/spibox/capture/" + capturename+"_%d.jpg" % (i)
         camerapid = subprocess.call(cmd,shell=True)
+        # taking video clip at 1080p30, 15Mbit/s, 25sec
+        cmd="raspivid -w 1920 -h 1080 -fps 30 -b 15000000 -n -t 25000 -o /home/pi/spibox/capture/" + capturename+"_%d.mp4" % (i)
+        camerapid = subprocess.call(cmd,shell=True)
+
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIR, GPIO.IN, GPIO.PUD_DOWN)
 
 try:
     print "Turning on motion sensor..."
- 
+
     # Loop until PIR indicates nothing is happening
     while GPIO.input(PIR)==1:
         Current_State  = 0
- 
+
     print "  Sensor ready"
- 
+
     while True:
         print('Waiting for movement')
         GPIO.wait_for_edge(PIR,GPIO.RISING)
@@ -36,4 +43,3 @@ except KeyboardInterrupt:
   print "  Bye for now"
   # Reset GPIO
   GPIO.cleanup()
-
